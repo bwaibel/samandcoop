83b80806c79ada83f19c500c7169ce0548556306
Bryn Waibel <bwaibel@brightcove.com>
2011-04-19 22:11:12 -0700

    hard code paths to files

--- wp-content/themes/samandcoop/header.php
+++ wp-content/themes/samandcoop/header.php
@@ -6,14 +6,14 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
 
-  <link rel="stylesheet" href="<?php get_stylesheet_directory_uri(); ?>/stylesheets/screen.css" type="text/css" media="screen, projection"> 
-  <link rel="stylesheet" href="<?php get_stylesheet_directory_uri(); ?>/stylesheets/print.css" type="text/css" media="print"> 
-  <!--[if lt IE 8]><link rel="stylesheet" href="<?php get_stylesheet_directory_uri(); ?>/stylesheets/ie.css" type="text/css" media="screen, projection"><![endif]--> 
+  <link rel="stylesheet" href="wp-content/themes/samandcoop/stylesheets/screen.css" type="text/css" media="screen, projection"> 
+  <link rel="stylesheet" href="wp-content/themes/samandcoop/stylesheets/print.css" type="text/css" media="print"> 
+  <!--[if lt IE 8]><link rel="stylesheet" href="wp-content/themes/samandcoop/stylesheets/ie.css" type="text/css" media="screen, projection"><![endif]--> 
 
   <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
 
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" type="text/javascript"></script>    
-  <script type="text/javascript" src="<?php get_stylesheet_directory_uri(); ?>/javascripts/main.js"></script>
+  <script type="text/javascript" src="wp-content/themes/samandcoop/avascripts/main.js"></script>
 
 
   <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
@@ -25,7 +25,7 @@
 
   <div id="header">
     <a href="<?php echo home_url(); ?>" id="home">
-      <img src="<?php get_stylesheet_directory_uri(); ?>/images/logo_03.png" alt="<?php bloginfo( 'name' ); ?>" />
+      <img src="wp-content/themes/samandcoop/images/logo_03.png" alt="<?php bloginfo( 'name' ); ?>" />
     </a>
   </div>
   
