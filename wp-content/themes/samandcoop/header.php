738cd2d14bb3d4d69d1d6b972d35fc4c0e2122e5
Bryn Waibel <bwaibel@brightcove.com>
2011-04-19 22:07:51 -0700

    moving some files

--- wp-content/themes/samandcoop/header.php
+++ wp-content/themes/samandcoop/header.php
@@ -1,45 +1,33 @@
-<?php
-/**
- * @package WordPress
- * @subpackage Clean Home
- */
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
-<!--[if IE]><link rel="stylesheet" href="<?php get_stylesheet_directory_uri(); ?>/ie.css" type="text/css" media="screen" /><![endif]-->
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-<?php wp_head(); ?>
+
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
+
+  <link rel="stylesheet" href="<?php get_stylesheet_directory_uri(); ?>/stylesheets/screen.css" type="text/css" media="screen, projection"> 
+  <link rel="stylesheet" href="<?php get_stylesheet_directory_uri(); ?>/stylesheets/print.css" type="text/css" media="print"> 
+  <!--[if lt IE 8]><link rel="stylesheet" href="<?php get_stylesheet_directory_uri(); ?>/stylesheets/ie.css" type="text/css" media="screen, projection"><![endif]--> 
+
+  <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" type="text/javascript"></script>    
+  <script type="text/javascript" src="<?php get_stylesheet_directory_uri(); ?>/javascripts/main.js"></script>
+
+
+  <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+  <?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
 
-<div id="wrapper">
-
-	<div class="header">
-		<div id="logo">
-		<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<h2><?php bloginfo( 'description' ); ?></h2>
-		</div>
-
-		<div id="nav">
-			<?php wp_nav_menu( array( 'container' => false, 'theme_location' => 'primary', 'fallback_cb' => 'cleanhome_page_menu' ) ); ?>
-		</div>
-		
-		<?php if ( '' != get_header_image() ) : ?>
-		<div id="header-image">
-			<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-		</div>
-		<?php endif; ?>
-	</div>
-
-	<?php if ( is_active_sidebar( 'header' ) ) : ?>
-	<div id="blurb">
-		<?php dynamic_sidebar( 'header' ); ?>
-	</div>
-	<?php endif; ?>
+  <div id="header">
+    <a href="<?php echo home_url(); ?>" id="home">
+      <img src="<?php get_stylesheet_directory_uri(); ?>/images/logo_03.png" alt="<?php bloginfo( 'name' ); ?>" />
+    </a>
+  </div>
+  
+  <div id="main">
+      <div id="head">
