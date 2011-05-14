762eb3e2fc111dda9ebeafe8202c70d03a9700ba
Bryn Waibel <bwaibel@brightcove.com>
2011-05-13 22:58:17 -0700

    forward

--- wp-content/themes/samandcoop/header.php
+++ wp-content/themes/samandcoop/header.php
@@ -32,7 +32,7 @@
     <div id="main">
         <div id="head">
             <ul id="nav">
-                <<li><a href="/">home</a></li>
+                <li><a href="/">home</a></li>
                 <?php shopp('catalog','category-list','depth=1&wraplist=off'); ?>
             </ul>
         </div>
